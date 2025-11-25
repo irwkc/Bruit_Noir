e11de6bf97d185a744145ddbd554e9de007a0772
Nikita Ognev <nikita@example.com>
2025-11-25 09:50:31 +0300

    Добавлен счетчик Яндекс.Метрики

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import SiteChrome from '@/components/SiteChrome'
 import MobileSiteChrome from '@/components/mobile/SiteChrome'
 import { Providers } from './providers'
+import Script from 'next/script'
 
 const inter = Inter({ 
   subsets: ['latin', 'cyrillic'],
@@ -32,6 +33,32 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${inter.className} flex flex-col min-h-dvh`}>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105470314', 'ym');
+
+            ym(105470314, 'init', {
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/105470314" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <Providers>
           {/* Desktop version */}
           <div className="hidden md:block">
