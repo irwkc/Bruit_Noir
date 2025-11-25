8eb22b8c31c560e4ba02de3604b4861d2cf98fec
Nikita Ognev <nikita@example.com>
2025-11-25 10:02:30 +0300

    Яндекс.Метрика: выполнен точный оф. сниппет

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,16 +33,18 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${inter.className} flex flex-col min-h-dvh`}>
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){
+        <Script
+          id="yandex-metrika"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){
               m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
               for (var j = 0; j < document.scripts.length; j++) {
                 if (document.scripts[j].src === r) { return; }
               }
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105470314', 'ym');
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
             ym(105470314, 'init', {
               ssr:true,
@@ -51,14 +53,15 @@ export default function RootLayout({
               ecommerce:"dataLayer",
               accurateTrackBounce:true,
               trackLinks:true
-            });
-          `}
-        </Script>
-        <noscript>
-          <div>
-            <img src="https://mc.yandex.ru/watch/105470314" style={{ position: 'absolute', left: '-9999px' }} alt="" />
-          </div>
-        </noscript>
+            });`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<div><img src="https://mc.yandex.ru/watch/105470314" style="position:absolute; left:-9999px;" alt="" /></div>',
+          }}
+        />
         <Providers>
           {/* Desktop version */}
           <div className="hidden md:block">
